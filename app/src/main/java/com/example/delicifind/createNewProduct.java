8ff6062c94925a755eabc7e5c34e88bdb11b7d8c
JIAMEI2000 <116060280+JIAMEI2000@users.noreply.github.com>
2023-11-01 15:38:58 +0800

    done insert image to firebase storage

--- app/src/main/java/com/example/delicifind/createNewProduct.java
+++ app/src/main/java/com/example/delicifind/createNewProduct.java
@@ -1,26 +1,114 @@
 package com.example.delicifind;
 
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 
 public class createNewProduct extends AppCompatActivity {
-    ImageView uploadImage;
+    ImageView imageView;
     Button selectBtn, uploadBtn;
+    StorageReference storageReference;
+    LinearProgressIndicator progressIndicator;
+    Uri image;
+    EditText productInput;
+    Spinner catSelectBox;
 
+    private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode()==RESULT_OK){
+                if(result.getData()!=null){
+                    image = result.getData().getData();
+                    uploadBtn.setEnabled(true);
+                    Glide.with(getApplicationContext()).load(image).into(imageView);
+                }
+            }else{
+                Toast.makeText(createNewProduct.this, "Please select an image", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_product);
 
+        FirebaseApp.initializeApp(createNewProduct.this);
+        storageReference = FirebaseStorage.getInstance().getReference();
+
         selectBtn = findViewById(R.id.selectFromGalleryBtn);
         uploadBtn = findViewById(R.id.uploadBtn);
-        uploadImage = findViewById(R.id.imageView);
+        imageView = findViewById(R.id.imageView);
+        progressIndicator = findViewById(R.id.progressBar);
+        catSelectBox = findViewById(R.id.catSelectBox);
+        productInput = findViewById(R.id.productInput);
 
+        selectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                activityResultLauncher.launch(intent);
+            }
+        });
+
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage(image);
+            }
+        });
+
+    }
+
+    private void uploadImage(Uri image){
+        StorageReference reference = storageReference.child("images/"+ UUID.randomUUID().toString());
+        reference.putFile(image).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(createNewProduct.this, "Image uploaded successfully!", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(createNewProduct.this, "There was an error while uploading image", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                progressIndicator.setMax(Math.toIntExact(snapshot.getTotalByteCount()));
+                progressIndicator.setProgress(Math.toIntExact(snapshot.getBytesTransferred()));
+            }
+        });
     }
 
 }
